3fec5f9bf4e36b5b8cd6cf374e45250f174a0fd9
Jiin Park <demon0315@naver.com>
2021-02-03 03:55:44 +0900

    posts app model end

---
+++ core/utils.py
@@ -0,0 +1,14 @@
+from uuid import uuid4
+
+
+def upload_to_uuid(instance, filename):
+    uuid_name = uuid4.hex
+    ext = filename.split(".")[-1]
+
+    return "/".join(
+        [
+            uuid_name[:2],
+            uuid_name[2:4],
+            uuid_name[4:] + ext,
+        ]
+    )
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from core.models import AbstractTimestamp
+from core.utils import upload_to_uuid
 
 # Create your models here.
+
+
+class Post(AbstractTimestamp):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    rate = models.PositiveSmallIntegerField(
+        default=5,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10),
+        ],
+    )
+    like = models.PositiveIntegerField(default=0)
+
+
+class Image(AbstractTimestamp):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=upload_to_uuid)
+
+
+class Comment(AbstractTimestamp):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    recommend = models.PositiveIntegerField(default=0)
