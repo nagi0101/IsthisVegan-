37abf1278ea80ac653a34175e5e665361199a3f5
Jiin Park <demon0315@naver.com>
2021-02-04 02:47:53 +0900

    add redirect to posts views

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from .models import Post, RatedPost, Comment
 import json
