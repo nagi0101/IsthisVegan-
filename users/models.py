2bd02a06e92f70699f9d413be2f93db5dbda8b55
eunhye <lime1pm@naver.com>
2021-02-03 13:42:08 +0900

    Add users app model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from core.models import AbstractTimestamp
 
-# Create your models here.
+class User(AbstractUser, AbstractTimestamp):
+    nickname = models.CharField(max_length=20)
+    bookmark = models.ManyToManyField("posts.Post", blank=True, related_name="users")
+    badge = models.ManyToManyField("users.Badge", related_name="users")
+
+    def __str__(self): 
+        return self.nickname
+
+class Badge(AbstractTimestamp):
+    grade = models.PositiveIntegerField(default=1)
+    name = models.CharField(max_length=50)
+
+    def __str__(self): 
+        return self.name
