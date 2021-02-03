ceed9e0539aca46448ae088e99c771e4f756e6b0
Jiin Park <demon0315@naver.com>
2021-02-03 14:14:46 +0900

    post category select

--- posts/models.py
+++ posts/models.py
@@ -7,10 +7,27 @@ from core.utils import upload_to_uuid
 
 
 class Post(AbstractTimestamp):
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="posts")
+
+    CATEGORY_INFO = ("INFO", "정보")
+    CATEGORY_COMMUNICATE = ("COMMUNICATE", "소통해요")
+    CATEGORY_VISIT = ("VISIT", "가봤어요")
+    CATEGORY_BUY = ("BUY", "사봤어요")
+
+    CATEGORY_SELECT = (
+        CATEGORY_INFO,
+        CATEGORY_COMMUNICATE,
+        CATEGORY_VISIT,
+        CATEGORY_BUY,
+    )
+
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="posts"
+    )
     title = models.CharField(max_length=120)
     content = models.TextField()
     like = models.PositiveIntegerField(default=0)
+    category = models.CharField(select=CATEGORY_SELECT, max_length=20)
+
 
 class RatedPost(Post):
     rate = models.PositiveSmallIntegerField(
@@ -20,12 +37,15 @@ class RatedPost(Post):
         ],
     )
 
+
 class Image(AbstractTimestamp):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images")
     image = models.ImageField(upload_to=upload_to_uuid)
 
 
 class Comment(AbstractTimestamp):
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="comments"
+    )
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     like = models.PositiveIntegerField(default=0)
