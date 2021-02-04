a188856c0a79c6541988039308187d26baa51580
heejin <dongdong7251@naver.com>
2021-02-04 10:33:01 +0900

    bookmark/mylist

--- users/urls.py
+++ users/urls.py
@@ -6,8 +6,8 @@ app_name = "users"
 
 urlpatterns = [
     path('', login, name='login'),
-  path("mypage/<int:pk>/",views.user_page, name="user_page"),
-  path("mypage/<int:pk>/bookmark/",views.user_bookmark, name="user_bookmark"),
-  path("mypage/<int:pk>/mylist/",views.user_mylist, name="user_mylist"),
+    path("mypage/<int:pk>/",views.user_page, name="user_page"),
+    path("mypage/<int:pk>/bookmark/",views.user_bookmark, name="user_bookmark"),
+    path("mypage/<int:pk>/mylist/",views.user_mylist, name="user_mylist"),
 
 ]
