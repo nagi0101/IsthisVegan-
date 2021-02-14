b15889d3705a2379399e4ae5115147761c5dbfb6
eunhye <lime1pm@naver.com>
2021-02-14 18:30:21 +0900

    FIX offline page setting

--- core/templates/serviceWorker.js
+++ core/templates/serviceWorker.js
@@ -1,21 +1,24 @@
 const STATIC_CACHE_NAME = "static-cache-v1";
 const STATIC_ASSETS = [
   "/",
+  "offline/",
   "/static/css/layout.css",
   "/static/css/reset.css",
   "/static/css/scrollbar.css",
+  "/static/js/manifest.json",
 ];
 
 const OFFLINE_CACHE_NAME = "offline-cache-v1";
-const OFFLINE_ASSETS = ["/offline"];
+const OFFLINE_ASSETS = ["offline/"];
 
 self.addEventListener("install", async (event) => {
   {% comment %} const staticCache = await caches.open(STATIC_CACHE_NAME);
   staticCache.addAll(STATIC_ASSETS); {% endcomment %}
-
-  const offlineCache = await caches.open(OFFLINE_CACHE_NAME);
-  offlineCache.addAll(OFFLINE_ASSETS);
+  
+  //const offlineCache = await caches.open(OFFLINE_CACHE_NAME);
+  //offlineCache.addAll(OFFLINE_ASSETS);
 });
+
 
 async function cacheFirst(req) {
   const cachedResponse = caches.match(req);
@@ -39,12 +42,22 @@ self.addEventListener("fetch", (event) => {
     // page navigation 제외
     return;
   }
-
+ 
+  /*
   event.respondWith(
     fetch(event.request).catch(() => {
       return caches
         .open(OFFLINE_CACHE_NAME)
         .then((cache) => cache.match("/offline"));
+    })
+  );
+  */
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches
+        .open(STATIC_CACHE_NAME)
+        .then((cache) => cache.match("offline/"));
     })
   );
 
