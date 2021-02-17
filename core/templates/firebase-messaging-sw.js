f8661945df20a2df25e2d1b303b650c0667618be
heejin <dongdong7251@naver.com>
2021-02-17 16:53:34 +0900

    firebase ing

--- core/templates/firebase-messaging-sw.js
+++ core/templates/firebase-messaging-sw.js
@@ -1,4 +1,16 @@
+// import firebase from "firebase/app";
+// import "firebase/messaging";
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../firebase-messaging-sw.js', { scope: "/firebase-messaging-sw.js" }
+  )
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+  }
+  
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyDARGGBG3LCA4d-9wfCQ7sGaP6jyjUmYZc",
